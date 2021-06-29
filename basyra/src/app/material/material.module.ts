efeaed4d8bbb7ab5833b108c3b35e541d4d27520
Adnan Latif <adnanflatif@gmail.com>
2021-06-29 19:40:29 +0500

    first view of banner and listing section is created

--- basyra/src/app/material/material.module.ts
+++ basyra/src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card'
 import { MatListModule } from '@angular/material/list';
 
 @NgModule({
@@ -19,6 +20,7 @@ import { MatListModule } from '@angular/material/list';
     MatInputModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule,
 
   ],
   exports:[
@@ -29,6 +31,7 @@ import { MatListModule } from '@angular/material/list';
     MatInputModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule,
 
   ],
 })
